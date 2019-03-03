3bcade7f5ccd126cfc6afea7cf26586a77284cce
Robert Tate <btate712@gmail.com>
2019-03-03 22:13:33 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
-# your code goes here
+def begins_with_r(array)
+  array.each do |word|
+    if word.chars.first != "r"
+      return false
+    end
+  end
+  return true
+end
+
+def contain_a(array)
+  return_array = []
+  array.each do |word|
+    if word.chars.include?("a")
+      return_array.push(word)
+    end
+  end
+  return_array
+end
+
+def first_wa(array)
+  array.each do |word|
+    if word.is_a?(String)
+      if (word.chars.first == "w") && (word.chars[1] == "a")
+        return word
+      end
+    end
+  end
+end
+
+def remove_non_strings(array)
+  return_String = []
+  array.each do |word|
+    if word.is_a?(String)
+      return_String.push(word)
+    end
+  end
+  array = return_String
+end
+
+def count_elements(array)
+  return_array = array.uniq
+  #initialize return_array value :count to 0 for each item
+  for i in 0..(return_array.length-1)
+    return_array[i][:count] = array.count(return_array[i])
+  end
+  return_array
+end
+
+def merge_data(keys, data)
+
+end
+  
